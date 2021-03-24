040a59c656301ac1ec7ceb58fd9cc88aaed26ef0
Oluwafikayo Ajayi <oluwafikayoajayi@gmail.com>
2021-03-24 16:28:01 +0100

    style components List

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const ListWrapper = styled.ul`
+    list-style:none;
+    text-align: left;
+    padding: 0;
+`;
+
+const ListItem = styled.li`
+    display: flex;
+    justify-content: space-between;
+`;
+
+const Label = styled.span`
+    font-weight: bold;
+`;
 
 const List = ({ items }) => (
-    <ul>
-        {items.map(item => <li key={item.label}>
-            <strong>{item.label}</strong> {item.value}
-        </li>)}
-    </ul>
+    <ListWrapper>
+        {items.map(item => <ListItem key={item.label}>
+            <Label>{item.label}</Label> {item.value}
+        </ListItem>)}
+    </ListWrapper>
 );
 
 export default List;
