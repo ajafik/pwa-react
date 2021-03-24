6fefd39b3681198e1a21d29340445bcbd1c1bbff
Oluwafikayo Ajayi <oluwafikayoajayi@gmail.com>
2021-03-24 16:43:54 +0100

    global style

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import styled  from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 import logo from '../GitHub-Mark-Light-64px.png';
 import Header from '../components/Header/Header';
 
 import Profile from './Profile';
 
+const GlobalStyle = createGlobalStyle`
+  body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+`;
 
 const AppWrapper = styled.div`
   text-align: center;
@@ -12,10 +23,14 @@ const AppWrapper = styled.div`
 
 function App() {
   return (
+    <>
+    <GlobalStyle />
     <AppWrapper>
       <Header logo={logo} />
-      <Profile />   
+      <Profile />
     </AppWrapper>
+
+    </>
   );
 }
 
